112a0cf69c4395aaec42ee219ee13d2b9f3d8ae5
bheema333 <127464698+bheema333@users.noreply.github.com>
2023-07-04 19:57:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn DevOps from Bheema
         </a>
       </header>
     </div>
